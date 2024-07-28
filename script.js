de79ea663e609f01566c05b70a49ae97ecf1df0c
Ignacio Guridi <nacho_g88@hotmail.com>
2024-07-28 17:34:56 -0300

    Agregado de carrousel logos

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        cssEase: 'linear'
+    });
+});
